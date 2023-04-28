c978c4a767b21d60f8c01bbbb58e0c89367f868e
RutvikWork <work.rrchampavat@gmail.com>
2023-04-28 12:33:24 +0530

    FIX: Changed the type of request

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,11 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export const GET = (req: NextApiRequest, res: NextApiResponse) => {
+export const GET = (req: Request) => {
   console.log("method", req.method);
   return new Response("Hello, there!");
-};
-
-export const POST = (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("post method", req.method);
-  return res.status(201).json({ "message": `The body value ` });
 };
