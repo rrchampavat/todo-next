4262164f23be785d2ebd7dd902853a3982eae091
RutvikWork <work.rrchampavat@gmail.com>
2023-05-04 13:14:02 +0530

    FIX: fetch changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Card from "@/components/card";
-import AddBtn from "./AddBtn";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/tasks");
-  const data: Task[] = await res.json();
+  const res = await fetch("http://localhost:3000/api/tasks", {
+    cache: "no-store",
+  });
+  const tasks: Task[] = await res.json();
 
   return (
     <main>
       <div className="grid grid-cols-1 md:grid-cols-4 place-content-between justify-between">
-        {data?.map((task, id) => (
+        {tasks?.map((task, id) => (
           <Card key={id} task={task} />
         ))}
       </div>
