8cd18bab9cbcca68ec7305407dfe7a221c50b7a9
RutvikWork <work.rrchampavat@gmail.com>
2023-05-10 13:37:01 +0530

    Changes

--- prisma/migrations/20230505045745_prisma_update_change/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `updated_at` on table `tasks` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "tasks" ALTER COLUMN "updated_at" SET NOT NULL;
--- prisma/migrations/20230508095747_removed_updated_at_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updated_at` on the `tasks` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "tasks" DROP COLUMN "updated_at";
